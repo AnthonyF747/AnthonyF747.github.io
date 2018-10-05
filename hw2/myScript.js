e7d73b63096d34f57131e6ffb4d179e08c859fe6
Anthony Franco <afranco15@wou.edu>
2018-10-05 12:42:28 -0700

    working on script for form1 button1

--- hw2/myScript.js
+++ hw2/myScript.js
@@ -1,8 +1,12 @@
-        $(function(){
-        $("button1").click(function(){
-            $("#name").text(function(origText){
-                $("#name-response").append("Welcome " + origText + " to my webpage!"){
-                }); 
-            });
-        });
+<script type="text/javascript"></script>
+
+$(function () 
+{
+    $("button1").click(function(e) {
+        var frm1val = $("#name").val();
+        var frm1sval = $("#season").val();
+
+        $("#name-response").append("Welcome " + frm1val + " !");
+        $("season-response").append("Your favorite season is " + frm1sval);
     });
+});
