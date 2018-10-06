f98a4f764cce6885db06ae05af96f881fb1b6eb4
Anthony Franco <afranco15@wou.edu>
2018-10-05 22:47:16 -0700

    added .js file, added code, deleted code, moved code, something that works in javascript

---
+++ hw2/myScript.js
@@ -0,0 +1,12 @@
+<script>
+    $(function() {
+
+        $("#button1").on("click", function() {
+            var btn1name = $(this).attr("name");
+            var btn1season = $(this).attr("season");
+
+            
+        });
+
+    });
+</script>
