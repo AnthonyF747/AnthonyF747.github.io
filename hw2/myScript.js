77af4cc0690fb8e6fdc113be8246f70f00e658db
Anthony Franco <afranco15@wou.edu>
2018-10-06 19:26:26 -0700

    got buttons to work

--- hw2/myScript.js
+++ hw2/myScript.js
@@ -1,17 +1,49 @@
-$(function() {
+$(document).ready(function() {
+    btn1();
+    convert();
+});
 
+function btn1() {
     var name1;
     var season1;
 
-    function btn1func() {
-        document.addEventListener('click', function () {
+    $('#button1').one('click', function () {
+        name1 = $('#name').val();
+        season1 = $('#season').val();
+        var ans = "Welcome " + name1 + "! ";
+        var seans = "Your favorite season is " + season1;
 
-            name1 = $('#name').val();
-            season1 = $('#season').val();
-            var nmrespon = "Welcome " + name1;
+        $('#response').append(ans + " " + seans);
 
-            $('#fid').append(nmrespon);
+        $('.input-control.input-sm').val('');
 
-        });
-    }
-});
+    });
+}
+
+function convert() {
+    var temp1;
+    var radval;
+    var ans;
+    var fans;
+
+    $('#button2').one('click', function() {
+        temp1 = $('#temp').val();
+        radval = $('input:radio[name=temperature]:checked').val();
+
+        if(!$.isNumeric(temp1)) {
+            alert("That is not a number.");
+        }
+        else {
+            if(radval == "fahrenheit") {
+                ans = (temp1 - 32) * .5556;
+                ans.toPrecision(2);
+                $('#frm2div').append(ans.toPrecision(2) + " C");
+            }
+            else if(radval == "celsius") {
+                ans = temp1 * 1.8 + 32;
+                $('#frm2div').append(ans.toPrecision(2) + " F");
+            }
+        }
+        $('#temp').val('');
+    });
+}
