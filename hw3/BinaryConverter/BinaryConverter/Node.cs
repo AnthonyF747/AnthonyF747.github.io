fd1c0e5981d6e88e7857f81bc613a7722137a62f
AnthonyF747 <afranco15@wou.edu>
2018-10-08 22:13:30 -0700

    created node class in old program class

---
+++ hw3/BinaryConverter/BinaryConverter/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryConverter
+
+    /// A single linked node class
+{
+    public class Node<T>
+    { 
+        public T data;                      // data is within the node
+        public Node<T> next;                // next is the next node (reference to)
+    
+        public Node(T data, Node<T> next)
+        {
+            this.data = data;              // current data 
+            this.next = next;              // current reference to next node
+        }
+    }
+}
