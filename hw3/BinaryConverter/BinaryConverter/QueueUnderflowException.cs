679b55d04753aefab37f25e69b823959fb92d328
AnthonyF747 <afranco15@wou.edu>
2018-10-10 11:32:22 -0700

    Added Exception class

---
+++ hw3/BinaryConverter/BinaryConverter/QueueUnderflowException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryConverter
+{
+    [Serializable]
+    class QueueUnderflowException : System.Exception
+    {
+        public QueueUnderflowException() : base() { }   // Default constructor
+        public QueueUnderflowException(string message) : base(message) { }    // Constructor that sets message property
+        public QueueUnderflowException(string message, System.Exception inner) : base(message, inner) { }   // Sets message and inner properties
+
+        // Constructor for Serialization; Exception propagates from remote server to client
+        protected QueueUnderflowException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context)
+        { }
+    }
+}
