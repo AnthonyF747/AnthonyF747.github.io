ea78896f22e5873575ca9fd92f1194c555adcf3f
AnthonyF747 <43625456+AnthonyF747@users.noreply.github.com>
2018-10-10 17:21:06 -0700

    more clean up

--- hw3/BinaryConverter/BinaryConverter/Node.cs
+++ hw3/BinaryConverter/BinaryConverter/Node.cs
@@ -1,16 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BinaryConverter
+{
 
     /// <summary>
     /// A single linked node class
     /// 
-    /// </summary>>
-{
+    /// </summary>
     public class LinkedListNode<T>
     { 
         public T Data { get; set; }                             // data is within the node
