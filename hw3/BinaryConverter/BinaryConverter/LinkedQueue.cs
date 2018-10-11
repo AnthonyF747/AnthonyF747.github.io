1e6585759a15c3e74e4106afb1b4c64a2a956271
AnthonyF747 <afranco15@wou.edu>
2018-10-10 20:48:11 -0700

    added code and comments to linkedqueue class

--- hw3/BinaryConverter/BinaryConverter/LinkedQueue.cs
+++ hw3/BinaryConverter/BinaryConverter/LinkedQueue.cs
@@ -6,17 +6,32 @@ using System.Threading.Tasks;
 
 namespace BinaryConverter
 {
-    class LinkedQueue<T> : IQueueInterface<T>
+    /// <summary>
+    /// A single linked FIFO queue
+    /// From Dale, Joyce, and Weems "Object-Oriented Data Structures Using Java
+    /// Modified by Scot Morse for CS460
+    /// 
+    /// This project is converting the modified Java version to a working C# version
+    /// @author Anthony Franco
+    /// @version 10/10/2018 Version 1.0
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class LinkedQueue<T> : IQueueInterface<T>           // LinkedList of generic type which implements IQueueInterface
+                                                        // which is a queue interface
     {
-        private Node<T> Front;
-        private Node<T> Rear;
+        private Node<T> Front;                          // The first node
+        private Node<T> Rear;                           // The last node
 
-        public LinkedQueue()
+        public LinkedQueue()                            // Constructor
         {
-            Front = null;
-            Rear = null;
+            Front = null;                               // Set Front to null
+            Rear = null;                                // Set Rear to null
         }
 
+        /// <summary>
+        /// Used to check if the list is empty
+        /// </summary>
+        /// <returns name="bool">Returns true is list is empty, otherwise false</returns>
         public bool IsEmpty()
         {
             if(Front == null && Rear == null)
@@ -27,51 +42,69 @@ namespace BinaryConverter
             {
                 return false;
             }
-            throw new NotImplementedException();
         }
 
         T IQueueInterface<T>.Pop => throw new NotImplementedException();
 
+        bool IQueueInterface<T>.IsEmpty => throw new NotImplementedException();
+
+        /// <summary>
+        /// Removes a node from the front of the queue
+        /// </summary>
+        /// <exception name="QueueUnderflowException">throw QueueUnderflowException if list is empty on pop() request</exception>>
+        /// <returns name="tmp">returns tmp node</returns>
         public T Pop()
         {
-            T tmp;
+            T tmp;                            // New node
 
-            if(IsEmpty())
+            if(IsEmpty())                     // Check if list is empty
             {
-                throw new QueueUnderflowException("The list is empty");
+                Exception message = null;     // Set up message for exception; if list is empty, throw exception with message
+                throw new QueueUnderflowException("Message: The list is empty\n{0}", message);
             }
-            else if(Front == Rear)
+            else if(Front == Rear)            // Check if there is one item in the list       
             {
-                tmp = Front.Data;
-                Front = null;
-                Rear = null;
+                tmp = Front.Data;             // If one node, assign that node to tmp
+                Front = null;                 // Set front to null
+                Rear = null;                  // Set rear to null
             }
-            else
+            else                              // General case
             {
-                tmp = Front.Data;
-                Front = Front.Next;
+                tmp = Front.Data;             // Assign the first node in the list to tmp
+                Front = Front.Next;           // The next node becomes the first node in the list
             }
-            return tmp;
+            return tmp;                       // Return tmp node
         }
+
+        /// <summary>
+        /// Use to push an element into the queue
+        /// </summary>
+        /// <param name="element"></param>
+        /// <exception name="NullReferenceException">Null element</exception>>
+        /// <returns name="element">Returns the element</returns>
         public T Push(T element)
         {
-            if(element == null)
+            if(element == null)                             // Check if there is a null element
             {
-                throw new NullReferenceException();
+                throw new NullReferenceException();         // System exception default
             }
 
-            if(IsEmpty())
+            if(IsEmpty())                                   // Check if the list is empty
             {
-                Node<T> tmp = new Node<T>(element, null);
-                Rear = Front = tmp;
+                Node<T> tmp = new Node<T>(element, null);   // If empty, make a new node
+                Rear = Front = tmp;                         // Make new node front and rear element in the list
             }
-            else
+            else                                            // General case
             {
-                Node<T> tmp = new Node<T>(element, null);
-                Rear.Next = tmp;
-                Rear = tmp;
+                Node<T> tmp = new Node<T>(element, null);   // Make a new node
+                Rear.Next = tmp;                            // Put new node at the end of the queue
+                Rear = tmp;                                 // Assign that node last element in the list
             }
-            return element;
+            return element;                                 // Return the element
+        }
+
+        T IQueueInterface<T>.Push(T element)
+        {
             throw new NotImplementedException();
         }
     }
