62940de43cf479bb33a2e596775a2e0dc8d67359
AnthonyF747 <afranco15@wou.edu>
2018-10-19 11:28:37 -0700

    Set up route for color controller

--- hw4/Project4/Project4/App_Start/RouteConfig.cs
+++ hw4/Project4/Project4/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace Project4
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "ColorChanger",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "ColorController", action = "ColorChooser", id = UrlParameter.Optional }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
