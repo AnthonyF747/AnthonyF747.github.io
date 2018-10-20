1e9b6997091d4d238ab882f2d095b0b1c3f86022
AnthonyF747 <afranco15@wou.edu>
2018-10-19 21:05:37 -0700

    Trying to get the colorchooser to get input and return any kind of response

--- hw4/Project4/Project4/Controllers/ColorController.cs
+++ hw4/Project4/Project4/Controllers/ColorController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,6 +13,30 @@ namespace Project4.Controllers
         // GET: Color
         public ActionResult ColorChooser()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ColorChooser(string firstcolor, string secondcolor)
+        {
+            if(firstcolor == null || secondcolor == null)
+            {
+                @ViewBag.Message = "Nothing is in here";
+            }
+            else
+            {
+                Debug.WriteLine(firstcolor);
+                Debug.WriteLine(secondcolor);
+                @ViewBag.Message = "something is here" + firstcolor + " " + secondcolor;
+            }
+            if (@ViewBag.Message != null)
+            {
+                @ViewBag.Message = "something is here" + firstcolor + " " + secondcolor;
+            }
+            else
+            {
+                @ViewBag.Message = "There's nothing here";
+            }
             return View();
         }
     }
