422ea0a11132e13060ea0f2bb457caaab95fd22c
AnthonyF747 <afranco15@wou.edu>
2018-10-20 21:14:35 -0700

    trying to fix what disappeared in the converter

--- hw4/Project4/Project4/Controllers/HomeController.cs
+++ hw4/Project4/Project4/Controllers/HomeController.cs
@@ -17,14 +17,34 @@ namespace Project4.Controllers
         [HttpGet]
         public ActionResult Converter()
         {
-            ViewBag.Message = "Your application description page.";
+            double miles = double.Parse(Request.QueryString["MilesToConvert"]);
+            var radbtn = Request.QueryString["Units"];
+            double sum = 0;
 
+           /* switch(radbtn)
+            {
+                case "millimeters":
+                    sum = miles * 1609344;
+                    break;
+                case "centimeters":
+                    sum = miles * 160934.4;
+                    break;
+                case "meters":
+                    sum = miles * 1609.344;
+                    break;
+                case "kilometers":
+                    sum = miles * 1.60934;
+                    break;
+                    
+            }
+            ViewBag.Message = miles + " miles is equal to " + sum + " " + radbtn;*/
             return View();
         }
 
         [HttpPost]
         public ActionResult Converter(int miles, string type)
         {
+            ViewBag.Message = "Your application description page.";
             return View();
         }
     }
