de49896bdb9894b81f2249731937a4c866ba2c1a
AnthonyF747 <afranco15@wou.edu>
2018-10-21 17:52:42 -0700

    still trying to build color chooser

--- hw4/Project4/Project4/Controllers/ColorController.cs
+++ hw4/Project4/Project4/Controllers/ColorController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -14,8 +16,34 @@ namespace Project4.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult ColorChooser()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ColorChooser(string x, string y)
+        {
+            Color colorOne;
+            Color colorTwo;
+            Regex regex = new Regex("^#[A-Fa-f0-9]{6} | [A-Fa-f0-9]{3}");
+
+            if(regex.IsMatch(x) && regex.IsMatch(y))
+            {
+                colorOne = ColorTranslator.FromHtml(x);
+                colorTwo = ColorTranslator.FromHtml(y);
+
+                ViewBag.NewColor = "it matches";
+            }
+            else
+            {
+                ViewBag.RegexFail = "The value entered was not is proper form. Try #AABBCC pattern";
+            }
+
+            
+            //Color newColor = colorOne + colorTwo;
+
             return View();
         }
     }
