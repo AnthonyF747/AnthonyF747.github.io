3c0f205a48a6d08a164bb4cacb4800fe973309ea
AnthonyF747 <afranco15@wou.edu>
2018-10-24 11:49:19 -0700

    changed index actionresult to viewresult

--- hw5/Project5/Project5/Controllers/HomeController.cs
+++ hw5/Project5/Project5/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace Project5.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ViewResult Index()
         {
             return View();
         }
