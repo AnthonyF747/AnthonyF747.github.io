f0ed6a28eb7b7e1e183d33b47c8507f62386e872
AnthonyF747 <afranco15@wou.edu>
2018-11-10 17:22:17 -0800

    Deleted default items from view/controller folders, fixed landing page; added textbox/button

---
+++ hw6/Project6/Project6/Project6/Models/ModelView1/PersonNameModelView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Project6.Models.ModelView1
+{
+    public class PersonNameModelView
+    {
+        public PersonNameModelView()
+            {
+                this.PersonIDModelViews = new List<PersonIDModelView>();
+            }
+        [Display(Name="Enter a name: ")]
+        public string NameOfPerson { get; set; }
+        public List<PersonIDModelView> PersonIDModelViews { get; set; }
+    }
+    
+}
