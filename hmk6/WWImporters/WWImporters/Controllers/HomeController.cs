184e434980da85115d85a670867840f9223f3f7e
AnthonyF747 <afranco15@wou.edu>
2018-11-16 15:00:35 -0800

    added image and comments to project

--- hmk6/WWImporters/WWImporters/Controllers/HomeController.cs
+++ hmk6/WWImporters/WWImporters/Controllers/HomeController.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/// HomeController for Homework 6
+/// This controller contains a private database object that
+/// will be used to collect information from the remote database.
+/// 
+/// Author: Anthony Franco
+/// Date: November 16, 2018
+/// Class: CS460 Software Engineering I
+/// Assignment: Homework 6
+/// Description: Learning to retrieve information from an 
+///              existing database using LINQ and Entity
+///              Framework.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -11,38 +22,57 @@ namespace WWImporters.Controllers
 {
     public class HomeController : Controller
     {
-        private WWIDbContext _wwiDb = new WWIDbContext();
+        private WWIDbContext _wwiDb = new WWIDbContext();  // database object
 
-        public ActionResult Index(string query)
+        /// <summary>
+        /// Gather input from the user via textbox to search for a 
+        /// name in the database.
+        /// </summary>
+        /// <param name="query">The name to search for; can be partial name</param>
+        /// <returns return="listofitems">A list of items that match the given input</returns>
+        public ActionResult Index(string query)            // query is input from the textbox
         {
-            if (query == null || query == "")
+            if (query == null || query == "")              // check if the query is null or empty;
             {
-                ViewBag.show = false;
-                return View();
+                ViewBag.show = false;                      // if true, return false
+                return View();                             // return original page
             }
-            else
+            else                                           // else,
             {
-                ViewBag.show = true;
+                ViewBag.show = true;                       // return true
             }
+                                                           // return a list of names containing input "query" from textbox
             return View(_wwiDb.People.Where(p => p.FullName.ToLower().Contains(query.ToLower())).ToList());
         }
 
-        public ActionResult PersonInfo(int? id)
+        /// <summary>
+        /// Used to retrieve specific information from the database
+        /// using LINQ queries. Once the information needed is found,
+        /// send it to the "PersonInfo" view.
+        /// </summary>
+        /// <param name="id">The id of the person being searched</param>
+        /// <returns return="PersonInfo">The view containing specific client information</returns>
+        public ActionResult PersonInfo(int? id)              // the id of the person being searched for
         {
-            GroupInfoView infoView = new GroupInfoView
+            GroupInfoView infoView = new GroupInfoView       // getters-setters for person, customer, and invoice objects
             {
-                ThisPerson = _wwiDb.People.Find(id)
+                ThisPerson = _wwiDb.People.Find(id)          // ThisPerson will equal the person in the database with the matching id
             };
-            if(id == null)
+            if(id == null)                                   // if the id is null,
             {
+                                                             // return badrequest error
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Person thisPerson = _wwiDb.People.Find(id);
-            if(thisPerson == null)
+
+            Person thisPerson = _wwiDb.People.Find(id);      // a person object matching person selected from the textbox list
+
+            if(thisPerson == null)                           // if the person returns null,
             {
-                return HttpNotFound();
+                return HttpNotFound();                       // throw not found error
             }
-            return View("PersonInfo", infoView);
+
+
+            return View("PersonInfo", infoView);             // return PersonInfo view 
         }
     }
 }
