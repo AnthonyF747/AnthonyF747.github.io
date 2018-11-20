706a7d6ffb6484fed92af3390e51162491eaa500
AnthonyF747 <afranco15@wou.edu>
2018-11-20 10:01:41 -0800

    working on MyScripts.js

--- hw7/Project7/Project7/Scripts/MyScripts.js
+++ hw7/Project7/Project7/Scripts/MyScripts.js
@@ -1,17 +1,40 @@
-﻿$(document).ready(function () {
-    $("input")
-        .keyup(function () {
-            var value = $(this).val();
-            $("#ptxt").text(value);
-        })
-        .keyup();
+﻿$(document).ready(function () {             // load functions when the document is loaded
+    inputText();
     cleardiv();
 });
 
-function cleardiv() {
-    $("button").click(function () {
-        $("#resultrow").empty();
-        $("#textbox").val('');
+function inputText() {                      // function for the textbox and displaying input
+    $("input").keypress(function (e) {      // a keypress function for the 'input' div (textbox)
+        var value = $(this).val();          // store the input value to variable 'value'
+        $("#ptxt").text(value);             // print out the value to the 'ptxt' div
+                                            // check if the input value is a space or spacebar element
+        if (e.key == " " || e.key == "spacebar") {
+            getWord();                      // call the getWord function to get a word
+            $("#testtxt").text("True");
+        }
+    })
+    .keyup();                               // call the function for every keyup
+}
+
+function cleardiv() {                       // function to clear the textbox and the 'resultrow' div
+    $("button").click(function () {         // trigger an event when the button is clicked
+        $("#resultrow").empty();            // set 'resultrow' to empty to clear div
+        $("#textbox").val('');              // set 'textbox' value to '' which clears the div
+    });
+}
+
+function getWord() {
+    var value = $(this).val();
+    var word = value.split(" ");
+    var preWord = word[word.length - 1];
+        
+    $("#texttxt").text(preWord);
+    $.ajax({
+        type: "Post",
+        dataType: "Json",
+        url: "../Translate/" + preWord,
+        success: functionName,
+        error: errorOnAjax
     });
 }
 
