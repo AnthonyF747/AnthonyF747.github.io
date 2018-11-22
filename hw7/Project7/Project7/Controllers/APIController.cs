c3b48e45a21d43047d135e96535a54aaec1e6e29
AnthonyF747 <afranco15@wou.edu>
2018-11-21 21:46:32 -0800

    set up key-value pair for giphy url in appSettings

--- hw7/Project7/Project7/Controllers/APIController.cs
+++ hw7/Project7/Project7/Controllers/APIController.cs
@@ -9,7 +9,8 @@ namespace Project7.Controllers
     public class APIController : Controller
     {
         private readonly string _myGiphyKey = System.Web.Configuration.WebConfigurationManager.AppSettings["AdminGiphy"];
-        
+        private readonly string _myGiphyURL = System.Web.Configuration.WebConfigurationManager.AppSettings["GiphyURL"];
+
         // GET: API
         public ActionResult Index()
         {
