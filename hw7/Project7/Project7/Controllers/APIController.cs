c0d47d48c5fb84a99c830dc0600b9d16faffe8a1
AnthonyF747 <afranco15@wou.edu>
2018-11-23 12:18:14 -0800

    working on controller code

--- hw7/Project7/Project7/Controllers/APIController.cs
+++ hw7/Project7/Project7/Controllers/APIController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,12 +11,28 @@ namespace Project7.Controllers
     public class APIController : Controller
     {
         private readonly string _myGiphyKey = System.Web.Configuration.WebConfigurationManager.AppSettings["AdminGiphy"];
-        private readonly string _myGiphyURL = System.Web.Configuration.WebConfigurationManager.AppSettings["GiphyURL"];
 
         // GET: API
         public ActionResult Index()
         {
             return View();
         }
+
+        public JsonResult GetGif (string inputData)
+        {
+            string _myGiphyURL = "http://api.giphy.com/v1/stickers/translate?api_key=" + _myGiphyKey + "&s=" + inputData;
+
+            WebRequest createURL = WebRequest.Create(_myGiphyURL);
+            HttpWebResponse response = (HttpWebResponse)createURL.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string formResponse = reader.ReadToEnd();
+            var serializeData = new System.Web.Script.Serialization.JavaScriptSerializer();
+            var jsonResponse = serializeData.DeserializeObject(formResponse);
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            return Json(jsonResponse, JsonRequestBehavior.AllowGet);
+        }
     }
-}
