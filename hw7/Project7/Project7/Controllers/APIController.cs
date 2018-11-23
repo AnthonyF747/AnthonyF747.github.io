28743d379cf917aa472b6266381197ac64d6ebfc
AnthonyF747 <afranco15@wou.edu>
2018-11-23 14:32:36 -0800

    added list of words to check through

--- hw7/Project7/Project7/Controllers/APIController.cs
+++ hw7/Project7/Project7/Controllers/APIController.cs
@@ -22,6 +22,7 @@ namespace Project7.Controllers
         {
             string _myGiphyURL = "http://api.giphy.com/v1/stickers/translate?api_key=" + _myGiphyKey + "&s=" + inputData;
 
+            if (commonWords.Contains(inputData.ToLower())) return Json(inputData, JsonRequestBehavior.AllowGet);
             WebRequest createURL = WebRequest.Create(_myGiphyURL);
             HttpWebResponse response = (HttpWebResponse)createURL.GetResponse();
             Stream dataStream = response.GetResponseStream();
@@ -35,5 +36,79 @@ namespace Project7.Controllers
             response.Close();
             return Json(jsonResponse, JsonRequestBehavior.AllowGet);
         }
+
+        private readonly List<string> commonWords = new List<string>
+        {
+            "a",
+            "about",
+            "after",
+            "all",
+            "also",
+            "always",
+            "am",
+            "and",
+            "another",
+            "any",
+            "anything",
+            "around",
+            "be",
+            "because",
+            "before",
+            "begin",
+            "behind",
+            "between",
+            "both",
+            "by",
+            "can't",
+            "come",
+            "do",
+            "don't",
+            "down",
+            "for",
+            "from",
+            "get",
+            "got",
+            "had",
+            "hadn't",
+            "has",
+            "hasn't",
+            "have",
+            "haven't",
+            "if",
+            "in",
+            "inside",
+            "is",
+            "isn't",
+            "it",
+            "just",
+            "kept",
+            "never",
+            "nor",
+            "not",
+            "other",
+            "out",
+            "outside",
+            "over",
+            "some",
+            "sometime",
+            "somewhere",
+            "that",
+            "the",
+            "this",
+            "through",
+            "throughout",
+            "to",
+            "too",
+            "under",
+            "use",
+            "up",
+            "was",
+            "wasn't",
+            "who",
+            "whose",
+            "you",
+            "your",
+            "you're",
+        };
     }
 }
